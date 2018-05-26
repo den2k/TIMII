ac10400982de5ee7b852d8c4fa130bb7c027124c
Daddy <daddy@eatons-macbook-air.local>
2018-05-26 09:38:12 -0700

    More progress on UI work. Added Logo Bar, ID Bar to Main view controller, all working with the Snapkit constraint library.  Doing this commit before moving UI code into separate files for better modularity and reuse.

---
+++ TIMII/Views/DashboardViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DashboardViewController.swift
+//  TIMII
+//
+//  Created by Daddy on 5/19/18.
+//  Copyright © 2018 Autonomii. All rights reserved.
+//
+
+import SnapKit
+
+class DashboardViewController : UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.lightGray
+
+        let topLeftBox = UIView()
+        view.addSubview(topLeftBox)
+        topLeftBox.backgroundColor = UIColor.blue
+        topLeftBox.snp.makeConstraints
+        { (make) -> Void in
+            // top, right, bottom, left
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).dividedBy(2)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).dividedBy(2)
+            make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).offset(16)
+        }
+        
+        let topRightBox = UIView()
+        view.addSubview(topRightBox)
+        topRightBox.backgroundColor = UIColor.orange
+        topRightBox.snp.makeConstraints
+        { (make) -> Void in
+            // top, right, bottom, left
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).offset(-16)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).dividedBy(2)
+            make.left.equalTo(topLeftBox.snp.right).offset(16)
+        }
+        
+    
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+    }
+
+    @objc func handleCancel()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- TIMII/Views/MainViewController.swift
+++ TIMII/Views/MainViewController.swift
@@ -6,27 +6,95 @@
 //  Copyright © 2018 Autonomii. All rights reserved.
 //
 
-import UIKit
+import SnapKit
 
 class MainViewController: UIViewController
 {
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.darkGray
+        view.backgroundColor = UIColor.init(rgb: 0xFCFCFC)
+        navigationController?.isNavigationBarHidden = true
 
-        // Just do this once after reseting the database to Zero Node
-        // then comment out. These setup the Countable Global variables.
-        // SetupSystem().GlobalServiceSetup()
+        // Logo Bar
+        let logoBar = UIView()
+        let logoHeaderHeight = 30
+        view.addSubview(logoBar)
+        logoBar.backgroundColor = UIColor.init(rgb: 0xFCFCFC)
+        logoBar.snp.makeConstraints
+            { (make) -> Void in
+                // top, right, bottom, left
+                make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+                make.right.equalTo(view.safeAreaLayoutGuide.snp.right)
+                make.height.equalTo(logoHeaderHeight)
+                make.left.equalTo(view.safeAreaLayoutGuide.snp.left)
+        }
+        
+        let appName = UILabel()
+        logoBar.addSubview(appName)
+        appName.textColor = UIColor.init(rgb: 0x404040)
+        appName.text = "TIMII"
+        appName.snp.makeConstraints
+            { (make) -> Void in
+                // top, right, bottom, left
+                make.centerX.equalTo(logoBar)
+                make.centerY.equalTo(logoBar)
+        }
+
+        // Family and Friends Bar
+        let FFBar = UIView()
+        let FFBarHeight = 80
+        view.addSubview(FFBar)
+        FFBar.backgroundColor = UIColor.init(rgb: 0xE5E5E5)
+        FFBar.snp.makeConstraints
+        { (make) -> Void in
+            // top, right, bottom, left
+            make.top.equalTo(logoBar.snp.bottom)
+            make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right)
+            make.height.equalTo(FFBarHeight)
+            make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left)
+        }
+        
+        // idCircleContainer
+        let idCircleContainer = UIView()
+        let idCircleContainerHeight = FFBarHeight - 1
+        FFBar.addSubview(idCircleContainer)
+        idCircleContainer.backgroundColor = UIColor.white
+        idCircleContainer.snp.makeConstraints
+        { (make) -> Void in
+            // top, right, bottom, left
+            make.centerY.equalTo(FFBar.snp.centerY)
+            make.right.equalTo(FFBar.snp.right)
+            make.height.equalTo(idCircleContainerHeight)
+            make.left.equalTo(FFBar.snp.left)
+        }
         
         
-        //  TODO: remove UUID use. Use sequential IDs or FB Push iDs or ....
-        let uid1 = UUID().uuidString
-        let board1 = DashboardSystem(uid1)
-        board1.addDashboard(uid1, "uid1 - 2nd Dashboard")
 
-        print(board1.UIType)
-        print(board1.isViewable)
+        
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Dashboard", style: .plain, target: self, action: #selector(handleDash))
         
     }
+    
+    @objc func handleDash()
+    {
+        // Transition with Back button to DashboardViewController
+        let dVC = DashboardViewController()
+        let navController = UINavigationController(rootViewController: dVC)
+        present(navController, animated: true, completion: nil)
+    }
 }
+
+// Add this to MainVC and just do this once after reseting the database to Zero Node
+// then comment out. These setup the Countable Global variables.
+// SetupSystem().GlobalServiceSetup()
+
+
+//  TODO: remove UUID use. Use sequential IDs or FB Push iDs or ....
+//        let uid1 = UUID().uuidString
+//        let board1 = DashboardSystem(uid1)
+//        board1.addDashboard(uid1, "uid1 - 2nd Dashboard")
+//
+//        print(board1.UIType)
+//        print(board1.isViewable)
+//
