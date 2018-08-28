a3fb690504af3d69ebc97df2c04bccf30d1d0a5c
Dennis Huang <den2k@ymail.com>
2018-08-28 08:17:09 -0700

    - Added Edit button to Expanded Day view.

--- TIMII/Views/TimelineScreen.swift
+++ TIMII/Views/TimelineScreen.swift
@@ -1,21 +1,21 @@
 //  Created by Daddy on 6/2/18.
 //  Copyright © 2018 Autonomii. All rights reserved.
 /*
-8.5.18
-This empty class is so the XML View Layout files can use the XML tag <HomeScreen> and reference the swift functions in this VC.
  
-TODO: 8.18.18 - DONE (8.23.18) Change to Moleskine-like Timeline view
-TODO: 8.23.18 - prefersStatusBarHidden for iPhone X does not work. This code was added to deal with X but its not working.
+ TODO: 8.18.18 - DONE (8.23.18) Change to Moleskine-like Timeline view
+ TODO: 8.25.18 - DONE (8.27.18) Expand day view when tapped.
+ TODO: 8.23.18 - prefersStatusBarHidden for iPhone X does not work. This code was added to deal with X but its not working.
+ TODO: 8.28.18 - Show dates in view.
+ TODO: 8.28.18 - Add Journal editing capability into this view ontap
+ 
+8.25.18
+Note: There is currently no safe way to explicitly bind a delegate to the layoutNode's owner class. Attempting
+to pass self as a constant or state variable will result in a retain cycle (which is why owner-binding is done
+implicitly rather than manually). This is why I'm getting a 'does not have an outlet named xxxxx' because the
+layout file cannot find TimelineScreen class. If you package custom components into a separate module then you will need
+to refer to them using their fully-qualified name in your XML.
 
-8.25.18 - Note: There is currently no safe way to explicitly bind a delegate to the layoutNode's owner class. Attempting to pass self as a constant or state variable will result in a retain cycle (which is why owner-binding is done implicitly rather than manually).
- 
- This is why I'm getting a 'does not have an outlet named xxxxx' because the layout file cannot find TimelineScreen class.
- 
- if you package custom components into a separate module then you will need to refer to them using their fully-qualified name in your XML.
- 
- */
-
-// TODO: 8.25.18 - DONE (8.27.18) Expand day view when tapped.
+*/
 
 import UIKit
 import Layout
@@ -53,6 +53,11 @@ class TimelineScreen: UIViewController, UIScrollViewDelegate, LayoutLoading
             isDayExpanded = true
             updateView()
         }
+    }
+    
+    @objc func editView()
+    {
+        print("Editing view.")
     }
     
     @objc private func updateView()
